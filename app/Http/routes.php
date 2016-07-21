577d8f29cb6b7f6fb18150310904b6851c1e96d9
cycdlm <cycdlm@gmail.com>
2016-07-21 16:12:28 +0800

    20160721new

---
+++ app/Http/routes.php
@@ -0,0 +1,62 @@
+<?php
+use App\Task;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/**
+ * 顯示所有任務
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy("created_at", "asc")->get();
+
+    return view("tasks", ["tasks" => $tasks,"another"=> "nihao"]);
+});
+
+/**
+ * 接收表單來新增任務
+ */
+//
+
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(),
+                                 ["name" => "required|max:255"]
+                                );
+
+//    if($validator->fails())
+//    {
+//        return "資料錯誤！！！";
+//    }
+    if($validator->fails())
+    {
+        return redirect("/")
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    
+    return redirect("/");
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect("/");
+});
+
